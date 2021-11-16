f314587ffded726c1ecf6b65e9145774d7cc78b0
Ephrem Wube <ephremworkeye@gmail.com>
2021-11-16 01:05:33 -0800

    added files and env file configured

--- sador_blog/settings.py
+++ sador_blog/settings.py
@@ -20,17 +20,22 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
+
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
+
+
+
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
@@ -138,6 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
