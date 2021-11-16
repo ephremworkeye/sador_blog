6ccf329c3b33dd85a1a9abd0a02f837cdd542c37
Ephrem Wube <ephremworkeye@gmail.com>
2021-11-16 01:29:43 -0800

    aws s3 configuration

--- sador_blog/settings.py
+++ sador_blog/settings.py
@@ -151,3 +151,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AWS_S3_ACCESS_KEY_ID = env('AWS_S3_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+
+
+
+AWS_S3_FILE_OVERWRITE  = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
